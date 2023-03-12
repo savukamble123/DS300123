ff5df4f606651cb261bc78be31c866c2846c1908
Savita Kamble <124324488+savukamble123@users.noreply.github.com>
2023-03-12 18:09:03 +0530

    Add files via upload

---
+++ Assignment_4/SquareFromList.py
@@ -0,0 +1,6 @@
+def Square_num(x):
+        return x*x
+    
+result = map(Square_num , [4, 5, 2, 9] )
+print("Square the elements of the list : ")
+print(list(result))
---
+++ Assignment_4/TrippleNo.py
@@ -0,0 +1,4 @@
+num = [1, 2, 3, 4, 5, 6, 7]
+result = map(lambda x: x + x + x, num) 
+print("Triple of list numbers: ")
+print(list(result))
